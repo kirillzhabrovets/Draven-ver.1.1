cb006ecca62e3dcbb90b8d3dacb5df27c8b60fbb
Kirill Zhabrovec <tanzanit.work@gmail.com>
2022-06-29 21:23:07 +0300

    fixed a bug + link React.

---
+++ app.js
@@ -0,0 +1,17 @@
+
+import './style.css'
+
+
+$(function() {
+    
+    let header = $("#header");
+    let intro = $("#intro")
+    let introH = intro.innerheight();
+    
+    console.log(introH);
+    
+    header.addClass("fixed");
+
+
+
+});
