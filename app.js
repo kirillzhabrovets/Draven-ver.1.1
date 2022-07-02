1748c40a9f71f72d346a1db9c948d6f38e37f8fb
Kirill Zhabrovec <tanzanit.work@gmail.com>
2022-07-02 20:06:03 +0300

    Add JS and burger menu.

--- app.js
+++ app.js
@@ -1,17 +1,79 @@
-
-import './style.css'
-
-
 $(function() {
-    
+
+
+    // Fixed Header//
+
     let header = $("#header");
     let intro = $("#intro")
-    let introH = intro.innerheight();
-    
-    console.log(introH);
-    
-    header.addClass("fixed");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+
+    checkScroll(scrollPos, introH);
+
+    $(window).on("scroll resize", function() {
+
+        let introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos, introH)
+    });
 
 
 
+    function checkScroll(scrollPos, introH) {
+
+        if( scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+
+    };
+
+
+    // Smoth scroll //
+
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 160
+        }, 700 );
+    })
+
+
+
+    // Nav Toggle//
+
+    $("#navToggle").on("click",function(event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+
+
+
+    // Reviews  https://kenwheeler.github.io/slick///
+
+    let slider = $("#reviewsSlider");
+
+        slider.slick({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            arrows: false,
+
+            cssEase: 'linear'
+        });
+                
 });
